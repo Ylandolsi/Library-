6d6148211c2c8e15df6bd6b5747d8e39432dbb9f
ylandolsi <yesslandolsi@gmail.com>
2025-02-24 15:22:07 +0100

    add dialoge for creating books

---
+++ script.js
@@ -0,0 +1,30 @@
+let gridItems = document.querySelectorAll('.grid-item');
+gridItems.forEach( item => {
+    item.addEventListener("mouseover", function() {
+        for (let i = 0; i < gridItems.length; i++) {
+            if ( gridItems[i] == item ) {
+                continue
+            }
+            gridItems[i].style.filter = 'blur(4px)'
+        }
+        item.style.transition = 'all 0.3s ease';
+        item.style.transform = 'scale(1.1)'
+    } ) 
+    item.addEventListener("mouseout", function() {
+        for ( let i = 0 ; i < gridItems.length ; i++ ) 
+        {
+            gridItems[i].style.filter = 'blur(0px)'
+        }
+        item.style.transform = 'scale(1)';
+    } ) ; 
+
+} );
+
+const dialog = document.querySelector("dialog");
+let addButtons= document.querySelectorAll('button');
+addButtons.forEach( button => {
+    button.addEventListener("click", function() {
+        dialog.showModal();
+
+    }) 
+}) ; 
