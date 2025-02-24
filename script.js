ba550cba6461e7b3f62cadaa30eced7225de7e4a
ylandolsi <yesslandolsi@gmail.com>
2025-02-24 16:34:06 +0100

    update

--- script.js
+++ script.js
@@ -27,15 +27,59 @@ let cancelButton = document.querySelector(".close-btn");
 let saveButton = document.querySelector(".submit");
 addButtons.forEach( button => {
     button.addEventListener("click", function() {
+        document.getElementById('bookName').value = '';
+        document.getElementById('imageUrl').value = '';
+        document.getElementById('description').value = '';
         dialog.showModal();
 
     }) 
 }) ; 
 
-// cancelButton.addEventListener("click", function() {
-//     dialog.close();
-// }
-// ); 
-// saveButton.addEventListener("click", function() {
-//     dialog.close();
-// });
+cancelButton.addEventListener("click", function() {
+
+    dialog.close();
+}
+); 
+
+function addBookToLibrary ( title, url ,description , read){
+    const newItem = document.createElement('div');
+    newItem.className = 'grid-item';
+
+//---------------------
+    const titleElement = document.createElement('h3');
+    titleElement.textContent = title;
+
+    const readStatus = document.createElement('p');
+    readStatus.textContent = read ? 'Read' : 'Not Read';
+
+
+    const descElement = document.createElement('p');
+    descElement.textContent = description;
+// ------------------------
+
+    const bookcover = document.createElement('div');
+    bookcover.className = 'bookcover';
+    const imgElement = document.createElement('img');
+    imgElement.src = url;
+    bookcover.appendChild(imgElement);
+
+    newItem.appendChild(bookcover);
+
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.insertBefore(newItem, gridContainer.lastElementChild);
+}
+saveButton.addEventListener("click", function() {
+    const title = document.getElementById('bookName').value;
+    const url = document.getElementById('imageUrl').value;
+    const description = document.getElementById('description').value;
+    const read = document.getElementById('isRead').checked;
+
+    if (title && url ) {
+        addBookToLibrary(title, url, description  ,read );
+    } else {
+        alert('Please fill in all required fields');
+        return;
+    }
+
+    dialog.close();
+});
